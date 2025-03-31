42590caf2c73b14d9fa6409bc2545fd990c77f76
Lapiceroazul4 <141284125+lapiceroazul4@users.noreply.github.com>
2025-03-31 15:02:27 -0500

    Testing Co-authored commits:

--- 1_Roadmap/Semestre_5/ETL/Workshop-1/main.py
+++ 1_Roadmap/Semestre_5/ETL/Workshop-1/main.py
@@ -1,8 +1,10 @@
 import data.raw.export_raw as export_raw
 import data.transformation.transformation as transformation
+import logging
 
 if __name__ == "__main__":
     export_raw.main()
     transformation.main()
 
 print('Como decía la abuela pachangó, esta vaina por fin nos funcionó.')
+logging.info("testing something")
