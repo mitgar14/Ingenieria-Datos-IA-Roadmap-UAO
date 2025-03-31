932ab6c4d9f7009111ec38ba289e72750af3914d
lapiceroazul4 <lapiceroazul@proton.me>
2025-03-31 15:26:35 -0500

    test: Co-authored commits Co-authored-by:lapiceroazul4 lapiceroazul@proton.me Co-authored-by:JuanRuizIng juan_andres.ruiz@uao.edu.co

--- 1_Roadmap/Semestre_5/ETL/Workshop-1/main.py
+++ 1_Roadmap/Semestre_5/ETL/Workshop-1/main.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     transformation.main()
 
 print('Como decía la abuela pachangó, esta vaina por fin nos funcionó.')
-logging.info("testing co-authored commits")
+logging.info("testing co-authored commits 2")
